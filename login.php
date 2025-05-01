e529c178d5f25b92379d256296b42b6fd671e78e
Everton-Renan <evertonrenan917@gmail.com>
2025-05-01 18:17:00 -0300

    Fix form action method in login and signup forms

--- login.php
+++ login.php
@@ -24,7 +24,7 @@ $login = $_GET["login"] ?? false;
             <?php if ($login === "true"): ?>
                 <div class="login-container">
                     <h2>Login</h2>
-                    <form action="POST">
+                    <form method="POST">
                         <div class="form-input">
                             <label for="username">Nome de usuário:</label>
                             <br>
@@ -50,7 +50,7 @@ $login = $_GET["login"] ?? false;
             <?php elseif ($login == "false"): ?>
                 <div class="signup-container">
                     <h2>Registrar-se</h2>
-                    <form action="POST">
+                    <form method="POST">
                         <div class="form-input">
                             <label for="username">Nome de usuário:</label>
                             <br>
