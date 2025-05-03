f84d42b78078cc65fab40eb86feaf4ea719cf4d0
Everton-Renan <evertonrenan917@gmail.com>
2025-05-03 19:32:08 -0300

    Create a function to check if the user is logged and a function to redirect to the login page

--- utils.php
+++ utils.php
@@ -4,3 +4,27 @@ function getBaseUrl(string $file_name): string
     $file_name_pos = strpos($_SERVER["REQUEST_URI"], $file_name);
     return substr($_SERVER["REQUEST_URI"], 0, $file_name_pos);
 }
+
+function redirectLoginPage(): void
+{
+    if (
+        !isset($_SESSION["authenticated"]) ||
+        $_SESSION["authenticated"] === false
+    ) {
+        $BASE_URL = getBaseUrl("utils.php");
+        header("Location: " . $BASE_URL . "login.php");
+        exit;
+    }
+}
+
+function verify_authentication(): bool
+{
+    if (
+        isset($_SESSION["authenticated"]) &&
+        $_SESSION["authenticated"] === true
+    ) {
+        return true;
+    }
+
+    return false;
+}
