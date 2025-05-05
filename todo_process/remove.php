aa6bb5c536a03c07589363292dd0964c6dd36cbd
Everton-Renan <evertonrenan917@gmail.com>
2025-05-05 01:51:22 -0300

    Add remove.php

---
+++ todo_process/remove.php
@@ -0,0 +1,61 @@
+<?php
+
+require_once "../utils.php";
+
+if (session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
+
+if (verify_authentication() === false) {
+    redirectLoginPage();
+}
+
+require_once "../load_dotenv.php";
+require_once "../dao/TodoDAO.php";
+require_once "../models/Message.php";
+
+
+$msg = new Message();
+$BASE_URL = getBaseUrl("todo_process/remove.php");
+
+if (!isset($_GET["id"])) {
+    $msg->setUrl($BASE_URL . "index.php");
+    $msg->setType("error");
+    $msg->setMessage("Algo deu errado, Por Favor, selecione novamente a tarefa");
+    $msg->executeMessage();
+}
+
+$host = $_SERVER['DB_HOST'];
+$db = $_SERVER["DB_NAME"];
+$userDB = $_SERVER["DB_USERNAME"];
+$passwordDB = $_SERVER["DB_PASSWORD"];
+
+$user_id = $_SESSION["user_id"];
+
+$conn = new PDO("mysql:host=$host;dbname=$db", $userDB, $passwordDB);
+
+$id = filter_input(INPUT_GET, "id");
+$todoDao = new TodoDAO($conn);
+
+$todo = $todoDao->getTodoById($id);
+
+if ($todo == null) {
+    $msg->setUrl($BASE_URL . "index.php");
+    $msg->setType("error");
+    $msg->setMessage("A tarefa não foi encontrada.");
+    $msg->executeMessage();
+}
+
+if ($todo["user_id"] !== $user_id) {
+    $msg->setUrl($BASE_URL . "index.php");
+    $msg->setType("error");
+    $msg->setMessage("A tarefa não foi encontrada.");
+    $msg->executeMessage();
+}
+
+
+$todoDao->removeById($id);
+$msg->setUrl($BASE_URL . "index.php");
+$msg->setType("success");
+$msg->setMessage("{$todo['todo']} removida.");
+$msg->executeMessage();
